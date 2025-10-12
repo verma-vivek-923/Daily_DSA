2b58039a16e316b7759638a0a9bfb8a0ea18b9a6
verma-vivek-923 <verma022003@gmail.com>
2025-10-12 22:05:18 +0530

    2nd solution for 2 sum

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
-class Solution {
+class Solution1 {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
+            //System.out.println(map);
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
                 return new int[]{map.get(complement), i}; // return indices
@@ -16,12 +17,29 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        // Check all pairs
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    // Return indices if sum matches
+                    return new int[]{i, j};
+                }
+            }
+        }
+        // If no pair found (LeetCode guarantees one)
+        return new int[0];
+    }
+}
+
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {2, 6, 7, 8, 3, 4, 5};
+        int[] arr = {2,7, 8, 3,6, 4, 5};
         int target = 8;
 
-        Solution obj1 = new Solution();
+        Solution1 obj1 = new Solution1();
+        Solution2 ob1 = new Solution2();
         int[] result = obj1.twoSum(arr, target);
 
         System.out.println("Indices: " + Arrays.toString(result));
