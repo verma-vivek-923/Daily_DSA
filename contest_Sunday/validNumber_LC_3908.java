47de7cd80d0b0d43ac5d324c5243fafb7befbb55
verma-vivek-923 <verma022003@gmail.com>
2026-05-25 07:54:07 +0530

    LC_3908 solve is number valide

--- contest_Sunday/validNumber_LC_3908.java
+++ contest_Sunday/validNumber_LC_3908.java
@@ -1,12 +1,36 @@
+import java.util.ArrayList;
+
 class Solution {
     public boolean validDigit(int n, int x) {
-        
-        
-         
-        return true;
+
+        String str = String.valueOf(n);
+
+        char ch = (char) (x + '0');
+
+        System.out.println(str);
+
+        if (str.charAt(0) == ch) {
+            return false;
+        }
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                System.out.println(str.charAt(i));
+                System.out.println(x);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
 public class validNumber_LC_3908 {
-    
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+
+        System.out.println(s1.validDigit(101, 0));
+        System.out.println(s1.validDigit(232, 2));
+        System.out.println(s1.validDigit(5, 1));
+    }
 }
