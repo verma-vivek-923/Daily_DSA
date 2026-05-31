f2a97050311ee55878269f94ce91b4e400937c43
verma-vivek-923 <verma022003@gmail.com>
2026-05-31 19:21:52 +0530

    LC_50 Power of x raise to n

---
+++ contest_Sunday/LC_50_power.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double pow=1;
+
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        
+        for(double i=0;i<n;i++){
+            pow*=x;
+        }
+
+        // pow=Math.pow(x, n);
+        return pow;
+    }
+}
+
+public class LC_50_power {
+    public static void main(String[] args) {
+        Solution s1=new Solution();
+        System.out.println(s1.myPow(2, 3));
+        System.out.println(s1.myPow(2, -2));
+    }
+}
