800a92c9ef7331d426f4baf3b0d77588e725b4b1
vivek verma <verma022003@gmail.com>
2026-06-09 21:51:40 +0530

    LC_338 Counting bit brute force apply

---
+++ Array/LC_338_countingBit.java
@@ -0,0 +1,28 @@
+//Brute Force Approach
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] countBits(int n) {
+         int ans[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            int k=0,m=i;
+                while(m>0){
+                    int rem=m%2;
+                      if(rem==1) k++;
+                      m/=2; 
+                }
+                ans[i]=k;
+        }
+        return ans;
+        
+    }
+}
+
+public class LC_338_countingBit {
+    public static void main(String[] args) {
+        Solution s1=new Solution();
+       System.out.println(Arrays.toString(s1.countBits(5)));
+       System.out.println(Arrays.toString(s1.countBits(2)));
+    }
+}
